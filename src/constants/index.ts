6ee2bced4d24f09718e1cb44ce154f9336dc6ea8
Kirill <krboktv@gmail.com>
2020-08-13 14:53:49 +0300

    update tokens

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -157,6 +157,6 @@ export const BETTER_TRADE_LINK_THRESHOLD = new Percent(JSBI.BigInt(75), JSBI.Big
 
 // the Uniswap Default token list lives here
 export const DEFAULT_TOKEN_LIST_URL =
-  'https://gateway.ipfs.io/ipfs/QmVJ5WpyS8JE8cp5crv58KiSiH56KqiHJbVtuFK7u7BAgn'
+  'https://gateway.ipfs.io/ipfs/QmeLhjAwvVtWGmjGP2CCEL8zLzKrLyB3K6YxJdGUEXFAFj'
 
 export const REFERRAL_ADDRESS_STORAGE_KEY = 'referral-address'
