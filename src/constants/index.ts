7f3932de60d46325a5ba748069a68d3b8c60f821
artall64 <artall64@gmail.com>
2020-11-18 18:28:05 +0200

    Updated default token list

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -157,6 +157,6 @@ export const BETTER_TRADE_LINK_THRESHOLD = new Percent(JSBI.BigInt(75), JSBI.Big
 
 // the Uniswap Default token list lives here
 export const DEFAULT_TOKEN_LIST_URL =
-  'https://gateway.ipfs.io/ipfs/QmWYKxfECNXCkoW3hNpLz8QdKHY9gYnxDPnRAWyPLAC4oD'
+  'https://gateway.ipfs.io/ipfs/Qmd8vFTBEP7rT9Ay7RNw9dErUMPGYKG3pKTnzrpErLsvwa'
 
 export const REFERRAL_ADDRESS_STORAGE_KEY = 'referral-address'
